53346ae8ae5557eaab51b44997e426745b4d0b91
Vinicius Chavier <109631057+viniciuschavier@users.noreply.github.com>
2023-11-08 10:26:35 -0300

    .

--- script.js
+++ script.js
@@ -17,6 +17,6 @@ function calcular(){
   if(resultado){
     document.getElementById('resultado').innerHTML = eval(resultado)
   }else{
-    document.getElementById('resultado').innerHTML = "ERRO!"
+    document.getElementById('resultado').innerHTML = "0"
   }
 }
